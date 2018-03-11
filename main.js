f1514eb42cda48b86849e855c38d30daeb3801a5
Allegra <aukseallegra@gmail.com>
2018-03-11 21:12:11 +0000

    edited nav, added jquery, changed image size

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-main
+
+$(document).ready(function(){ 
+
+$("#fa-chevron-down").hover(function(){
+  $("#scroll-down").slideToggle("fast");
+  // $('#fa-chevron-down').hide();
+});
+
+// $("#scroll-down").hover(function(){
+//   $("#fa-chevron-down").slideToggle("slow");
+//   // $('#scroll-down').hide();
+// });
+
+
+})
