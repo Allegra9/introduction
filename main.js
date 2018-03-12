5e8ba5ff3bf0439b74fabe9fd2d3d2d7adde7203
Allegra <aukseallegra@gmail.com>
2018-03-12 21:54:15 +0000

    ipad parallax trial

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 
-$(document).ready(function(){ 
+// $(document).ready(function(){ 
 
-$("#fa-chevron-down").hover(function(){
-  $("#scroll-down").slideToggle("fast");
-  // $('#fa-chevron-down').hide();
-});
-
-// $("#scroll-down").hover(function(){
-//   $("#fa-chevron-down").slideToggle("slow");
-//   // $('#scroll-down').hide();
+// $("#fa-chevron-down").hover(function(){
+//   $("#scroll-down").slideToggle("fast");
+//   // $('#fa-chevron-down').hide();
 // });
 
+// // $("#scroll-down").hover(function(){
+// //   $("#fa-chevron-down").slideToggle("slow");
+// //   // $('#scroll-down').hide();
+// // });
 
-})
+
+// })
